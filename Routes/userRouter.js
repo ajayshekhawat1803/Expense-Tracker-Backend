2e8e7aabb4796d01fd91b4363a122097b3c94f2f
ajayshekhawat1803 <ajayshekhawat1803@gmail.com>
2023-10-29 09:38:15 +0530

    Added user login section

--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -8,10 +8,15 @@ userRouter.get("/", (req, res) => {
 })
 
 userRouter.post("/register", async (req, res) => {
-    const { name, username, password,email } = req.body
-    const userToRegister = new userModel({ name, username, password,email });
+    const { name, username, password, email } = req.body
+    const userToRegister = new userModel({ name, username, password, email });
     const result = await userToRegister.save()
     res.json(result)
+})
+userRouter.post("/login", async (req, res) => {
+    const { username, password } = req.body
+    const userToLogin = await userModel.findOne({ username: username, password: password });
+    res.json(userToLogin)
 })
 
 export default userRouter;
